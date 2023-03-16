e3e03f0bc97e24d1d14088b4e1394666e713d6d0
Louis Payette <payette1996@gmail.com>
2023-03-16 10:53:21 -0400

    Page 2 fini

---
+++ class/classVoitureManager.php
@@ -0,0 +1,50 @@
+<?php
+class VoitureManager
+{
+    private PDO $pdo;
+
+    public function __construct(?PDO $pdo = NULL)
+    {
+        if (!$pdo) {
+            $dsn = (string) "mysql:host=127.0.0.1;dbname=dblocation";
+            $this->pdo = new PDO($dsn, "root", "");
+        }
+        else {
+            $this->pdo = $pdo;
+        }
+    }
+
+    public function getVoitures(?int $id = NULL) : string|array
+    {
+        $sql = "
+            SELECT * FROM tblvoiture AS v
+            LEFT JOIN tblmarque AS m ON v.idMarque = m.idMarque
+            LEFT JOIN tblcategorie AS c ON v.idCategorie = c.idCategorie
+        ";
+
+
+        if (!$id) {
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute();
+            $voitures = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            $listVoitures = (string) "";
+            foreach ($voitures as $voiture) {
+                $listVoitures .= "
+                    <li><a href='./voitures.php?idVoiture={$voiture['idVoiture']}'>{$voiture['marque']} {$voiture['modele']}</a></li>
+                ";
+            }
+
+            return $listVoitures;
+        } else {
+            $sql .= "WHERE idVoiture = :idVoiture";
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->bindValue(":idVoiture", $id);
+            $stmt->execute();
+            $voiture = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            return $voiture;
+        }
+    }
+}
+?>
--- voitures.php
+++ voitures.php
@@ -4,34 +4,29 @@
 
     require_once './inc/header.php';
     require_once './inc/car-info.php';
+    require_once "./class/classVoitureManager.php";
+
+    $voitureManager = new VoitureManager();
 
     echo '<h1>Voitures disponibles</h1>';
-
     if (isset($_GET['idVoiture'])) {
-        $voitureObj = selectCarById($_GET['idVoiture'], CAR_INFO);
-
-        echo '<article class="grille">
-                <h2>' . $voitureObj->get_marque() . ' ' . $voitureObj->get_modele() . ' - ' .
-                    $voitureObj->get_categorie() . '</h2>
-                <img src="./inc/img/' . $voitureObj->get_image() . '" alt="' . $voitureObj->get_marque() .
-                    ' ' . $voitureObj->get_modele() . '" />
-                <p><strong>Passager :</strong> ' . $voitureObj->get_nbPassager() . '</p>
-                <p><strong>Description :</strong> ' . $voitureObj->get_description() . '</p>
-              </article>
-              <p class="retour"><a href="./voitures.php">Retour à la liste des voitures</a></p>';
+        $voiture = $voitureManager->getVoitures($_GET['idVoiture']);
+        echo
+            "
+                <article class='grille'>
+                    <h2>{$voiture['marque']} {$voiture['modele']} {$voiture['categorie']}</h2>
+                    <img src='./inc/img/{$voiture['image']}' alt='{$voiture['marque']} {$voiture['modele']}'>
+                    <p><strong>Passager : </strong>{$voiture['nbPassager']}</p>
+                    <p><strong>Description : </strong>{$voiture['description']}</p>
+                </article>
+                <p class='retour'><a href='./voitures.php'>Retour à la liste des voitures</a></p>
+            ";
     }
 
     else {
-        echo '<h2>Sélectionnez une voiture pour en savoir plus</h2>
-              <ul class="car_list">';
-
-        for ($i = 0; $i < sizeof(CAR_INFO); $i++) {
-            $voitureObj = selectCarById(($i + 1), CAR_INFO);
-
-            echo '<li><a href="./voitures.php?idVoiture=' . $voitureObj->get_id() . '">' .
-                    $voitureObj->get_marque() . ' ' . $voitureObj->get_modele() . '</a></li>';
-        }
-    
+        echo'<h2>Sélectionnez une voiture pour en savoir plus</h2>
+            <ul class="car_list">';
+        echo $voitureManager->getVoitures();
         echo '</ul>';
     }
 
